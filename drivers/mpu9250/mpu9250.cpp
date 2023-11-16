1826cf8f52cae0731f3d293f86f31a194032d72c
Cindy Li <cindyli0213@hotmail.com>
2023-11-15 21:54:06 -0500

    clang format

--- drivers/mpu9250/mpu9250.cpp
+++ drivers/mpu9250/mpu9250.cpp
@@ -1,175 +1,190 @@
 #include "mpu9250.h"
 
 void MPU9250::init(const Config& config) {
-    esp_err_t ret;
-    spi_device_interface_config_t devcfg = {
-        .command_bits = 0,
-        .address_bits = 8,
-        .dummy_bits = 0,
-        .mode = 3,
-        .clock_source = SPI_CLK_SRC_DEFAULT,
-        .duty_cycle_pos = 0,
-        .cs_ena_pretrans = 0,
-        .cs_ena_posttrans = 0,
-        .clock_speed_hz = config.spi_clock_speed_hz,
-        .input_delay_ns = 0,
-        .spics_io_num = config.cs_pin,
-        .flags = 0,
-        .queue_size = 8,
-        .pre_cb = nullptr,
-        .post_cb = nullptr
-    };
+  esp_err_t ret;
+  spi_device_interface_config_t devcfg = {
+      .command_bits = 0,
+      .address_bits = 8,
+      .dummy_bits = 0,
+      .mode = 3,
+      .clock_source = SPI_CLK_SRC_DEFAULT,
+      .duty_cycle_pos = 0,
+      .cs_ena_pretrans = 0,
+      .cs_ena_posttrans = 0,
+      .clock_speed_hz = config.spi_clock_speed_hz,
+      .input_delay_ns = 0,
+      .spics_io_num = config.cs_pin,
+      .flags = 0,
+      .queue_size = 8,
+      .pre_cb = nullptr,
+      .post_cb = nullptr};
 
-    ret = spi_bus_add_device(spi_host_, &devcfg, &spi_handle_);
-    ESP_ERROR_CHECK(ret);
+  ret = spi_bus_add_device(spi_host_, &devcfg, &spi_handle_);
+  ESP_ERROR_CHECK(ret);
 
-    reset();
-    set_sleep(false);
-    disable_i2c(false);
-    set_clock_source(ClockSource::PLL);
+  reset();
+  set_sleep(false);
+  disable_i2c(false);
+  set_clock_source(ClockSource::PLL);
 
-    // Verify device whoami
-    ret = (whoami() == DEFAULT_WHOAMI ? ESP_OK : ESP_ERR_INVALID_RESPONSE);
-    ESP_ERROR_CHECK(ret);
+  // Verify device whoami
+  ret = (whoami() == DEFAULT_WHOAMI ? ESP_OK : ESP_ERR_INVALID_RESPONSE);
+  ESP_ERROR_CHECK(ret);
 
-    set_fsr(config.accel_fsr, config.gyro_fsr);
-    set_dlpf(config.accel_dlpf, config.gyro_dlpf);
-    set_sample_rate(config.sample_rate_hz);
+  set_fsr(config.accel_fsr, config.gyro_fsr);
+  set_dlpf(config.accel_dlpf, config.gyro_dlpf);
+  set_sample_rate(config.sample_rate_hz);
 }
 
 void MPU9250::read(RawImuData& imu_data) {
-    uint8_t buffer[14] = {0};
-    read_bytes(Register::ACCEL_XOUT_H, 14, buffer);
-    imu_data.accel.x = (static_cast<uint16_t>(buffer[0]) << 8) | static_cast<uint16_t>(buffer[1]);
-    imu_data.accel.y = (static_cast<uint16_t>(buffer[2]) << 8) | static_cast<uint16_t>(buffer[3]);
-    imu_data.accel.z = (static_cast<uint16_t>(buffer[4]) << 8) | static_cast<uint16_t>(buffer[5]);
-    imu_data.temperature = (static_cast<uint16_t>(buffer[6]) << 8) | static_cast<uint16_t>(buffer[7]);
-    imu_data.gyro.x = (static_cast<uint16_t>(buffer[8]) << 8) | static_cast<uint16_t>(buffer[9]);
-    imu_data.gyro.y = (static_cast<uint16_t>(buffer[10]) << 8) | static_cast<uint16_t>(buffer[11]);
-    imu_data.gyro.z = (static_cast<uint16_t>(buffer[12]) << 8) | static_cast<uint16_t>(buffer[13]);
+  uint8_t buffer[14] = {0};
+  read_bytes(Register::ACCEL_XOUT_H, 14, buffer);
+  imu_data.accel.x = (static_cast<uint16_t>(buffer[0]) << 8) |
+                     static_cast<uint16_t>(buffer[1]);
+  imu_data.accel.y = (static_cast<uint16_t>(buffer[2]) << 8) |
+                     static_cast<uint16_t>(buffer[3]);
+  imu_data.accel.z = (static_cast<uint16_t>(buffer[4]) << 8) |
+                     static_cast<uint16_t>(buffer[5]);
+  imu_data.temperature = (static_cast<uint16_t>(buffer[6]) << 8) |
+                         static_cast<uint16_t>(buffer[7]);
+  imu_data.gyro.x = (static_cast<uint16_t>(buffer[8]) << 8) |
+                    static_cast<uint16_t>(buffer[9]);
+  imu_data.gyro.y = (static_cast<uint16_t>(buffer[10]) << 8) |
+                    static_cast<uint16_t>(buffer[11]);
+  imu_data.gyro.z = (static_cast<uint16_t>(buffer[12]) << 8) |
+                    static_cast<uint16_t>(buffer[13]);
 }
 
 void MPU9250::reset() {
-    write_bit(Register::PWR_MGMT_1, static_cast<uint8_t>(PwrMgmt1Bits::H_RESET), 1);
-    write_bit(Register::USER_CTRL, static_cast<uint8_t>(UserCtrlBits::SIG_COND_RST), 1);
+  write_bit(Register::PWR_MGMT_1, static_cast<uint8_t>(PwrMgmt1Bits::H_RESET),
+            1);
+  write_bit(Register::USER_CTRL,
+            static_cast<uint8_t>(UserCtrlBits::SIG_COND_RST), 1);
 }
 
 void MPU9250::set_sleep(bool sleep) {
-    write_bit(Register::PWR_MGMT_1, static_cast<uint8_t>(PwrMgmt1Bits::SLEEP), sleep);
+  write_bit(Register::PWR_MGMT_1, static_cast<uint8_t>(PwrMgmt1Bits::SLEEP),
+            sleep);
 }
 
 void MPU9250::disable_i2c(bool disable) {
-    write_bit(Register::USER_CTRL, static_cast<uint8_t>(UserCtrlBits::I2C_IF_DIS), disable);
+  write_bit(Register::USER_CTRL, static_cast<uint8_t>(UserCtrlBits::I2C_IF_DIS),
+            disable);
 }
 
 void MPU9250::set_clock_source(ClockSource clock_source) {
-    write_bits(Register::PWR_MGMT_1, static_cast<uint8_t>(PwrMgmt1Bits::CLK_SEL_LSB), 3, static_cast<uint8_t>(clock_source));
+  write_bits(Register::PWR_MGMT_1,
+             static_cast<uint8_t>(PwrMgmt1Bits::CLK_SEL_LSB), 3,
+             static_cast<uint8_t>(clock_source));
 }
 
 uint8_t MPU9250::whoami() {
-    return read_reg(Register::WHOAMI);
+  return read_reg(Register::WHOAMI);
 }
 
 void MPU9250::set_fsr(AccelFSR accel_fsr, GyroFSR gyro_fsr) {
-    write_bits(Register::ACCEL_CONFIG, static_cast<uint8_t>(AccelConfigBits::FS_SEL_LSB), 2, static_cast<uint8_t>(accel_fsr));
-    write_bits(Register::GYRO_CONFIG, static_cast<uint8_t>(GyroConfigBits::FS_SEL_LSB), 2, static_cast<uint8_t>(gyro_fsr));
+  write_bits(Register::ACCEL_CONFIG,
+             static_cast<uint8_t>(AccelConfigBits::FS_SEL_LSB), 2,
+             static_cast<uint8_t>(accel_fsr));
+  write_bits(Register::GYRO_CONFIG,
+             static_cast<uint8_t>(GyroConfigBits::FS_SEL_LSB), 2,
+             static_cast<uint8_t>(gyro_fsr));
 }
 
 void MPU9250::set_dlpf(AccelDLPF accel_dlpf, GyroDLPF gyro_dlpf) {
-    if (accel_dlpf == AccelDLPF::NONE) {
-        write_bit(Register::ACCEL_CONFIG_2, static_cast<uint8_t>(AccelConfig2Bits::F_CHOICE_B), 1);
-    }
-    else {
-        write_bit(Register::ACCEL_CONFIG_2, static_cast<uint8_t>(AccelConfig2Bits::F_CHOICE_B), 0);
-        write_bits(Register::ACCEL_CONFIG_2, static_cast<uint8_t>(AccelConfig2Bits::A_DLPFCFG_LSB), 3, static_cast<uint8_t>(accel_dlpf));
-    }
+  if (accel_dlpf == AccelDLPF::NONE) {
+    write_bit(Register::ACCEL_CONFIG_2,
+              static_cast<uint8_t>(AccelConfig2Bits::F_CHOICE_B), 1);
+  } else {
+    write_bit(Register::ACCEL_CONFIG_2,
+              static_cast<uint8_t>(AccelConfig2Bits::F_CHOICE_B), 0);
+    write_bits(Register::ACCEL_CONFIG_2,
+               static_cast<uint8_t>(AccelConfig2Bits::A_DLPFCFG_LSB), 3,
+               static_cast<uint8_t>(accel_dlpf));
+  }
 
-    if (gyro_dlpf == GyroDLPF::NONE) {
-        write_bits(Register::GYRO_CONFIG, static_cast<uint8_t>(GyroConfigBits::F_CHOICE_B_LSB), 2, 0b11);
-    }
-    else {
-        write_bits(Register::GYRO_CONFIG, static_cast<uint8_t>(GyroConfigBits::F_CHOICE_B_LSB), 2, 0b00);
-        write_bits(Register::CONFIG, static_cast<uint8_t>(ConfigBits::DLPF_CFG_LSB), 3, static_cast<uint8_t>(gyro_dlpf));
-    }
+  if (gyro_dlpf == GyroDLPF::NONE) {
+    write_bits(Register::GYRO_CONFIG,
+               static_cast<uint8_t>(GyroConfigBits::F_CHOICE_B_LSB), 2, 0b11);
+  } else {
+    write_bits(Register::GYRO_CONFIG,
+               static_cast<uint8_t>(GyroConfigBits::F_CHOICE_B_LSB), 2, 0b00);
+    write_bits(Register::CONFIG, static_cast<uint8_t>(ConfigBits::DLPF_CFG_LSB),
+               3, static_cast<uint8_t>(gyro_dlpf));
+  }
 }
 
 void MPU9250::set_sample_rate(uint16_t rate_hz) {
-    // TODO: assume 1kHz internal sample rate
-    constexpr uint16_t INTERNAL_SAMPLE_RATE_HZ = 1000;
-    uint8_t divider = INTERNAL_SAMPLE_RATE_HZ / rate_hz - 1;
-    write_reg(Register::SMPLRT_DIV, divider);
+  // TODO: assume 1kHz internal sample rate
+  constexpr uint16_t INTERNAL_SAMPLE_RATE_HZ = 1000;
+  uint8_t divider = INTERNAL_SAMPLE_RATE_HZ / rate_hz - 1;
+  write_reg(Register::SMPLRT_DIV, divider);
 }
 
 uint8_t MPU9250::read_reg(Register addr) {
-    esp_err_t ret;
-    uint8_t rx_buffer;
+  esp_err_t ret;
+  uint8_t rx_buffer;
 
-    spi_transaction_t transaction = {
-        .flags = 0,
-        .cmd = 0,
-        .addr = static_cast<uint8_t>(addr) | 0x80U,
-        .length = 8,
-        .rxlength = 8,
-        .user = nullptr,
-        .tx_buffer = nullptr,
-        .rx_buffer = &rx_buffer
-    };
+  spi_transaction_t transaction = {.flags = 0,
+                                   .cmd = 0,
+                                   .addr = static_cast<uint8_t>(addr) | 0x80U,
+                                   .length = 8,
+                                   .rxlength = 8,
+                                   .user = nullptr,
+                                   .tx_buffer = nullptr,
+                                   .rx_buffer = &rx_buffer};
 
-    ret = spi_device_polling_transmit(spi_handle_, &transaction);
-    ESP_ERROR_CHECK(ret);
+  ret = spi_device_polling_transmit(spi_handle_, &transaction);
+  ESP_ERROR_CHECK(ret);
 
-    return rx_buffer;
+  return rx_buffer;
 }
 
 void MPU9250::read_bytes(Register addr, size_t len, uint8_t* buffer) {
-    esp_err_t ret;
-    spi_transaction_t transaction = {
-        .flags = 0,
-        .cmd = 0,
-        .addr = static_cast<uint8_t>(addr) | 0x80U,
-        .length = len * 8,
-        .rxlength = len * 8,
-        .user = nullptr,
-        .tx_buffer = nullptr,
-        .rx_buffer = buffer
-    };
+  esp_err_t ret;
+  spi_transaction_t transaction = {.flags = 0,
+                                   .cmd = 0,
+                                   .addr = static_cast<uint8_t>(addr) | 0x80U,
+                                   .length = len * 8,
+                                   .rxlength = len * 8,
+                                   .user = nullptr,
+                                   .tx_buffer = nullptr,
+                                   .rx_buffer = buffer};
 
-    ret = spi_device_polling_transmit(spi_handle_, &transaction);
-    ESP_ERROR_CHECK(ret);
+  ret = spi_device_polling_transmit(spi_handle_, &transaction);
+  ESP_ERROR_CHECK(ret);
 }
 
 void MPU9250::write_reg(Register addr, uint8_t data) {
-    esp_err_t ret;
+  esp_err_t ret;
 
-    spi_transaction_t transaction = {
-        .flags = 0,
-        .cmd = 0,
-        .addr = static_cast<uint8_t>(addr),
-        .length = 8,
-        .rxlength = 8,
-        .user = nullptr,
-        .tx_buffer = &data,
-        .rx_buffer = nullptr
-    };
+  spi_transaction_t transaction = {.flags = 0,
+                                   .cmd = 0,
+                                   .addr = static_cast<uint8_t>(addr),
+                                   .length = 8,
+                                   .rxlength = 8,
+                                   .user = nullptr,
+                                   .tx_buffer = &data,
+                                   .rx_buffer = nullptr};
 
-    ret = spi_device_polling_transmit(spi_handle_, &transaction);
-    ESP_ERROR_CHECK(ret);
+  ret = spi_device_polling_transmit(spi_handle_, &transaction);
+  ESP_ERROR_CHECK(ret);
 }
 
 void MPU9250::write_bit(Register addr, uint8_t bit, bool set) {
-    uint8_t reg_val = read_reg(addr);
-    reg_val &= ~(0x01U << bit);
-    reg_val |= (static_cast<uint8_t>(set) << bit);
-    write_reg(addr, reg_val);
+  uint8_t reg_val = read_reg(addr);
+  reg_val &= ~(0x01U << bit);
+  reg_val |= (static_cast<uint8_t>(set) << bit);
+  write_reg(addr, reg_val);
 }
 
 void MPU9250::write_bits(Register addr, uint8_t lsb, size_t len, uint8_t data) {
-    uint8_t reg_val = read_reg(addr);
-    uint8_t mask = 0;
-    for (uint8_t i = lsb; i < lsb+len; i++) {
-        mask |= (0x01U << i);
-    }
-    reg_val &= ~mask;
-    reg_val |= (data << lsb) & mask;
-    write_reg(addr, reg_val);
+  uint8_t reg_val = read_reg(addr);
+  uint8_t mask = 0;
+  for (uint8_t i = lsb; i < lsb + len; i++) {
+    mask |= (0x01U << i);
+  }
+  reg_val &= ~mask;
+  reg_val |= (data << lsb) & mask;
+  write_reg(addr, reg_val);
 }
--- examples/mpu9250_test/main/mpu9250_test.cpp
+++ examples/mpu9250_test/main/mpu9250_test.cpp
@@ -1,16 +1,16 @@
-#include <stdio.h>
+#include "mpu9250.h"
 #include <inttypes.h>
-#include "sdkconfig.h"
+#include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "mpu9250.h"
+#include "sdkconfig.h"
 
-#define SPI_HOST     SPI2_HOST
+#define SPI_HOST SPI2_HOST
 
 #define PIN_NUM_MISO 13
 #define PIN_NUM_MOSI 11
-#define PIN_NUM_CLK  12
-#define PIN_NUM_CS   10
+#define PIN_NUM_CLK 12
+#define PIN_NUM_CS 10
 
 static constexpr MPU9250::AccelFSR ACCEL_FSR = MPU9250::AccelFSR::_8G;
 static constexpr MPU9250::GyroFSR GYRO_FSR = MPU9250::GyroFSR::_1000DPS;
@@ -22,52 +22,50 @@ static constexpr MPU9250::Config IMU_CONFIG = {
     .accel_fsr = ACCEL_FSR,
     .gyro_fsr = GYRO_FSR,
     .accel_dlpf = MPU9250::AccelDLPF::_218_1HZ,
-    .gyro_dlpf = MPU9250::GyroDLPF::_184HZ
-};
+    .gyro_dlpf = MPU9250::GyroDLPF::_184HZ};
 
 extern "C" {
 
 void app_main(void) {
-    printf("MPU 9250 Test\n");
+  printf("MPU 9250 Test\n");
 
-    esp_err_t ret;
-    spi_bus_config_t buscfg = {
-        .mosi_io_num = PIN_NUM_MOSI,
-        .miso_io_num = PIN_NUM_MISO,
-        .sclk_io_num = PIN_NUM_CLK,
-        .quadwp_io_num = -1,
-        .quadhd_io_num = -1,
-        .data4_io_num = -1,
-        .data5_io_num = -1,
-        .data6_io_num = -1,
-        .data7_io_num = -1,
-        .max_transfer_sz = 4092,
-        .flags = 0,
-        .isr_cpu_id = ESP_INTR_CPU_AFFINITY_AUTO,
-        .intr_flags = 0
-    };
+  esp_err_t ret;
+  spi_bus_config_t buscfg = {.mosi_io_num = PIN_NUM_MOSI,
+                             .miso_io_num = PIN_NUM_MISO,
+                             .sclk_io_num = PIN_NUM_CLK,
+                             .quadwp_io_num = -1,
+                             .quadhd_io_num = -1,
+                             .data4_io_num = -1,
+                             .data5_io_num = -1,
+                             .data6_io_num = -1,
+                             .data7_io_num = -1,
+                             .max_transfer_sz = 4092,
+                             .flags = 0,
+                             .isr_cpu_id = ESP_INTR_CPU_AFFINITY_AUTO,
+                             .intr_flags = 0};
 
-    ret = spi_bus_initialize(SPI_HOST, &buscfg, SPI_DMA_CH_AUTO);
-    ESP_ERROR_CHECK(ret);
+  ret = spi_bus_initialize(SPI_HOST, &buscfg, SPI_DMA_CH_AUTO);
+  ESP_ERROR_CHECK(ret);
 
-    MPU9250 imu(SPI_HOST);
-    imu.init(IMU_CONFIG);
+  MPU9250 imu(SPI_HOST);
+  imu.init(IMU_CONFIG);
 
-    MPU9250::RawImuData imu_data;
+  MPU9250::RawImuData imu_data;
 
-    while (true) {
-        imu.read(imu_data);
-        printf("accel_x %.3f accel_y %.3f accel_z %.3f gyro_x %.3f gyro_y %.3f gyro_z %.3f temp %.3f\n", 
-                MPU9250::ACCEL_RAW_TO_G(imu_data.accel.x, ACCEL_FSR),
-                MPU9250::ACCEL_RAW_TO_G(imu_data.accel.y, ACCEL_FSR),
-                MPU9250::ACCEL_RAW_TO_G(imu_data.accel.z, ACCEL_FSR),
-                MPU9250::GYRO_RAW_TO_DPS(imu_data.gyro.x, GYRO_FSR),
-                MPU9250::GYRO_RAW_TO_DPS(imu_data.gyro.y, GYRO_FSR),
-                MPU9250::GYRO_RAW_TO_DPS(imu_data.gyro.z, GYRO_FSR),
-                MPU9250::TEMPERAURE_RAW_TO_DEG_C(imu_data.temperature));
+  while (true) {
+    imu.read(imu_data);
+    printf(
+        "accel_x %.3f accel_y %.3f accel_z %.3f gyro_x %.3f gyro_y %.3f gyro_z "
+        "%.3f temp %.3f\n",
+        MPU9250::ACCEL_RAW_TO_G(imu_data.accel.x, ACCEL_FSR),
+        MPU9250::ACCEL_RAW_TO_G(imu_data.accel.y, ACCEL_FSR),
+        MPU9250::ACCEL_RAW_TO_G(imu_data.accel.z, ACCEL_FSR),
+        MPU9250::GYRO_RAW_TO_DPS(imu_data.gyro.x, GYRO_FSR),
+        MPU9250::GYRO_RAW_TO_DPS(imu_data.gyro.y, GYRO_FSR),
+        MPU9250::GYRO_RAW_TO_DPS(imu_data.gyro.z, GYRO_FSR),
+        MPU9250::TEMPERAURE_RAW_TO_DEG_C(imu_data.temperature));
 
-        vTaskDelay(10 / portTICK_PERIOD_MS);
-    }
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+  }
 }
-
 }
