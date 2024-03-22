4fb4c20db08cfb1b1ee8a99ddeacce879e103acc
Cindy Li <cindyli0213@hotmail.com>
2024-03-21 22:50:10 -0400

    working gui

--- software/host/gui.py
+++ software/host/gui.py
@@ -1,6 +1,6 @@
 import sys, signal, time
 import pyqtgraph as pg
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QGridLayout, QLabel, QPushButton, QGraphicsDropShadowEffect
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QGridLayout, QLabel, QPushButton, QLineEdit, QDialog, QDialogButtonBox, QFormLayout
 from PyQt6.QtGui import QFont
 from PyQt6.QtCore import QTimer, pyqtSignal
 from enum import Enum
@@ -11,7 +11,6 @@ class State(Enum):
     Idle = 2
     Active = 3
     Actuated = 4
-    Unknown = 5
 
 
 VBATT_100_PERCENT = 4.2
@@ -22,12 +21,41 @@ VBATT_LOW_THRESHOLD = 3.2
 g_window = None
 g_app = None
 g_app_exit = False
+g_calibrate_requested = False
+g_new_config_params = None # tuple: (inversion_threshold_dps, actuated_timeout_s, idle_variance_threshold_g2, idle_transition_time_s)
+g_new_config_requested = False
+
+class UpdateConfigDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.setWindowTitle('Update config parameters')
+
+        self.inversion_threshold = QLineEdit(self)
+        self.actuated_timeout = QLineEdit(self)
+        self.idle_variance_threshold = QLineEdit(self)
+        self.idle_transition_time = QLineEdit(self)
+        button_box = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel, self);
+
+        layout = QFormLayout(self)
+        layout.addRow('Inversion threshold (dps)', self.inversion_threshold)
+        layout.addRow('Actuated timeout (s)', self.actuated_timeout)
+        layout.addRow('Idle variance threshold (G^2)', self.idle_variance_threshold)
+        layout.addRow('Idle transition time (s)', self.idle_transition_time)
+        layout.addWidget(button_box)
+
+        button_box.accepted.connect(self.accept)
+        button_box.rejected.connect(self.reject)
+
+    def get_new_config_params(self):
+        return (self.inversion_threshold.text(), self.actuated_timeout.text(), self.idle_variance_threshold.text(), self.idle_transition_time.text())
 
 
 class MainWindow(QMainWindow):
     vbatt_signal = pyqtSignal(float)
     inversion_speed_signal = pyqtSignal(float)
     state_signal = pyqtSignal(State)
+    update_config_signal = pyqtSignal(float, float, float, float)
 
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -40,6 +68,21 @@ class MainWindow(QMainWindow):
             border-radius: 12px;
             padding: 6px;
         '''
+        self.BUTTON_PRESSED_STYLESHEET = '''
+            background-color: #3B3B3B;
+            border-style: outset;
+            border-width: 1px;
+            border-radius: 12px;
+            padding: 8px;
+        '''
+        self.BUTTON_RELEASED_STYLESHEET = '''
+            background-color: #6B6B6B;
+            border-style: outset;
+            border-width: 1px;
+            border-color: #7B7B7B;
+            border-radius: 12px;
+            padding: 8px;
+        '''
 
         self.setWindowTitle('Dynamic Ankle Support')
         # self.setStyleSheet('background-color: #2C6FBB') # blue
@@ -48,12 +91,14 @@ class MainWindow(QMainWindow):
         self.vbatt_signal.connect(self.update_battery)
         self.inversion_speed_signal.connect(self.update_plot)
         self.state_signal.connect(self.update_state)
+        self.update_config_signal.connect(self.update_config)
 
         layout_parent = QHBoxLayout()
         layout_left = QVBoxLayout()
         layout_right = QVBoxLayout()
         layout_state = QHBoxLayout()
         layout_button = QHBoxLayout()
+        layout_config_label = QHBoxLayout()
         layout_config = QGridLayout()
         layout_battery = QGridLayout()
 
@@ -71,39 +116,40 @@ class MainWindow(QMainWindow):
 
         self.calibrate_button = QPushButton('Calibrate')
         self.calibrate_button.setFont(self.LABEL_FONT)
-        self.calibrate_button.setStyleSheet('''
-            background-color: #6B6B6B;
-            border-style: outset;
-            border-width: 1px;
-            border-color: #7B7B7B;
-            border-radius: 12px;
-            padding: 8px;
-        ''')
-        shadow = QGraphicsDropShadowEffect(self)
-        shadow.setOffset(2, 3)
-        shadow.setBlurRadius(10)
-        self.calibrate_button.setGraphicsEffect(shadow)
-        self.calibrate_button.clicked.connect(self.calibrate_button_clicked)
+        self.calibrate_button.setStyleSheet(self.BUTTON_RELEASED_STYLESHEET)
+        self.calibrate_button.pressed.connect(self.calibrate_button_pressed)
+        self.calibrate_button.released.connect(self.calibrate_button_released)
 
-        spacer1 = QLabel()
+        spacer = QLabel()
 
         layout_button.addWidget(self.calibrate_button, stretch=0)
-        layout_button.addWidget(spacer1, stretch=1)
+        layout_button.addWidget(spacer, stretch=1)
 
         config_label = QLabel()
         config_label.setText('Config')
         config_label.setFont(self.LABEL_FONT)
         config_label.setFixedSize(100, 40)
         config_label.setStyleSheet(self.LABEL_STYLESHEET)
+        layout_config_label.addWidget(config_label, stretch=0)
+        
+        self.update_config_dialog = UpdateConfigDialog()
+
+        self.update_config_button = QPushButton('Update')
+        self.update_config_button.setFont(QFont('Helvetica', 14, QFont.Weight.ExtraLight))
+        self.update_config_button.setFixedSize(70, 36)
+        self.update_config_button.setStyleSheet(self.BUTTON_RELEASED_STYLESHEET)
+        self.update_config_button.pressed.connect(self.update_config_button_pressed)
+        self.update_config_button.released.connect(self.update_config_button_released)
+        layout_config_label.addWidget(spacer, stretch=1)
+        layout_config_label.addWidget(self.update_config_button, stretch=0)
 
         inversion_threshold_label = QLabel()
         inversion_threshold_label.setText('Inversion Threshold')
         inversion_threshold_label.setFont(self.LABEL_FONT)
         inversion_threshold_label.setStyleSheet(self.LABEL_STYLESHEET)
         layout_config.addWidget(inversion_threshold_label, 0, 0)
-        
+
         self.inversion_threshold = QLabel()
-        self.inversion_threshold.setText('300 °/s')
         self.inversion_threshold.setFont(self.VALUE_FONT)
         layout_config.addWidget(self.inversion_threshold, 0, 1)
         
@@ -114,10 +160,9 @@ class MainWindow(QMainWindow):
         layout_config.addWidget(actuated_timeout_label, 1, 0)
         
         self.actuated_timeout = QLabel()
-        self.actuated_timeout.setText('2.5 s')
         self.actuated_timeout.setFont(self.VALUE_FONT)
         layout_config.addWidget(self.actuated_timeout, 1, 1)
-        
+
         idle_variance_threshold_label = QLabel()
         idle_variance_threshold_label.setText('Idle Variance Threshold')
         idle_variance_threshold_label.setFont(self.LABEL_FONT)
@@ -125,7 +170,6 @@ class MainWindow(QMainWindow):
         layout_config.addWidget(idle_variance_threshold_label, 2, 0)
         
         self.idle_variance_threshold = QLabel()
-        self.idle_variance_threshold.setText('1.2 G^2')
         self.idle_variance_threshold.setFont(self.VALUE_FONT)
         layout_config.addWidget(self.idle_variance_threshold, 2, 1)
         
@@ -136,16 +180,13 @@ class MainWindow(QMainWindow):
         layout_config.addWidget(idle_transition_time_label, 3, 0)
         
         self.idle_transition_time = QLabel()
-        self.idle_transition_time.setText('60 s')
         self.idle_transition_time.setFont(self.VALUE_FONT)
         layout_config.addWidget(self.idle_transition_time, 3, 1)
 
-        spacer2 = QLabel()
-
         layout_left.addLayout(layout_state, stretch=1)
         layout_left.addLayout(layout_button, stretch=1)
-        layout_left.addWidget(spacer2, stretch=1)
-        layout_left.addWidget(config_label, stretch=1)
+        layout_left.addWidget(spacer, stretch=1)
+        layout_left.addLayout(layout_config_label, stretch=1)
         layout_left.addLayout(layout_config)
         layout_left.setContentsMargins(0,40,0,10)
 
@@ -250,9 +291,43 @@ class MainWindow(QMainWindow):
             self.battery_percentage.setStyleSheet('color: #EE4B2B')
             self.vbatt_value.setStyleSheet('color: #EE4B2B')
 
-    def calibrate_button_clicked(self):
-        print('calibrate')
+    def update_config(self, inversion_threshold, actuated_timeout, idle_variance_threshold, idle_transition_time):
+        self.inversion_threshold.setText('{} °/s'.format(int(inversion_threshold)))
+        self.actuated_timeout.setText('{:.2f} s'.format(actuated_timeout))
+        self.idle_variance_threshold.setText('{:.2f} G^2'.format(idle_variance_threshold))
+        self.idle_transition_time.setText('{:.2f} s'.format(idle_transition_time))
 
+    def calibrate_button_pressed(self):
+        global g_calibrate_requested
+        g_calibrate_requested = True
+        self.calibrate_button.setStyleSheet(self.BUTTON_PRESSED_STYLESHEET)
+    
+    def calibrate_button_released(self):
+        self.calibrate_button.setStyleSheet(self.BUTTON_RELEASED_STYLESHEET)
+
+    def update_config_button_pressed(self):
+        self.update_config_button.setStyleSheet(self.BUTTON_PRESSED_STYLESHEET)
+        if self.update_config_dialog.exec():
+            inversion_threshold, actuated_timeout, idle_variance_threshold, idle_transition_time = self.update_config_dialog.get_new_config_params()
+            try:
+                global g_new_config_params
+                global g_new_config_requested
+                inversion_threshold = float(inversion_threshold.strip())
+                actuated_timeout = float(actuated_timeout.strip())
+                idle_variance_threshold = float(idle_variance_threshold.strip())
+                idle_transition_time = float(idle_transition_time.strip())
+                g_new_config_params = (inversion_threshold, actuated_timeout, idle_variance_threshold, idle_transition_time)
+                g_new_config_requested = True
+            except ValueError:
+                pass
+        self.update_config_button_released()
+
+    def update_config_button_released(self):
+        self.update_config_button.setStyleSheet(self.BUTTON_RELEASED_STYLESHEET)
+
+
+
+########## PUBLIC APIS ##########
 
 def g_update_inversion_speed(inversion_speed: float):
     global g_window
@@ -267,6 +342,13 @@ def g_update_state(state: State):
 def g_update_vbatt(vbatt: float):
     global g_window
     g_window.vbatt_signal.emit(vbatt)
+
+
+def g_update_config(inversion_threshold_dps: float, actuated_timeout_s: float, idle_variance_threshold_g2: float, idle_transition_time_s: float):
+    global g_window
+    global g_new_config_requested
+    g_window.update_config_signal.emit(inversion_threshold_dps, actuated_timeout_s, idle_variance_threshold_g2, idle_transition_time_s)
+    g_new_config_requested = False
 
 
 def g_init_gui():
@@ -288,3 +370,22 @@ def g_run_gui():
 def g_gui_exited():
     global g_app_exit
     return g_app_exit
+
+
+def g_is_calibration_requested():
+    global g_calibrate_requested
+    return g_calibrate_requested
+
+
+def g_clear_calibration_request():
+    global g_calibrate_requested
+    g_calibrate_requested = False
+
+
+def g_maybe_get_new_config_params():
+    global g_new_config_params
+    global g_new_config_requested
+    if g_new_config_requested:
+        return g_new_config_params
+    else:
+        return None
--- software/host/main.py
+++ software/host/main.py
@@ -6,9 +6,14 @@ def user_thread():
         counter = 0
         vbatt = 4.2
         states = [State.Calibrating, State.Idle, State.Active, State.Actuated]
+
+        g_update_config(300, 2.5, 1.2, 60)
+
         while not g_gui_exited():
             counter += 1
+
             g_update_inversion_speed(random.random() * 100.0)
+
             if counter >= 10:
                 g_update_state(random.choice(states))
                 g_update_vbatt(vbatt)
@@ -16,15 +21,25 @@ def user_thread():
                 vbatt -= 0.01
                 if vbatt <= 0.0:
                     vbatt = 4.2
+
+            if g_is_calibration_requested():
+                g_clear_calibration_request()
+                print('Calibration requested')
+
+            new_config_params = g_maybe_get_new_config_params()
+            if new_config_params is not None:
+                print('New config requested:', new_config_params)
+                g_update_config(new_config_params[0], new_config_params[1], new_config_params[2], new_config_params[3])
+
             time.sleep(0.1)
 
 
 if __name__ == '__main__':
-    g_init_gui()
+    g_init_gui() # must be called first in the main thread
 
     t = threading.Thread(target=user_thread)
     t.start()
 
-    g_run_gui()
+    g_run_gui() # must be called in the main thread after starting all other threads
 
     t.join()
